768d99c4ea0c6e36472c90acd2ca23b2555cc3b3
saeid mosavi <themsvz@gmail.com>
2023-09-05 02:23:41 +0330

    this two sesions ...

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import "./App.css";
+const tabData = [
+    {
+        id: 1,
+        title: "ITEM 1",
+        content: "Authorize the user data ✅",
+    },
+    {
+        id: 2,
+        title: "ITEM 2",
+        content: "Redirect user to cart page 🛒",
+    },
+    {
+        id: 3,
+        title: "ITEM 3",
+        content: "Create new payment for the user 💰",
+    },
+];
+
+function App() {
+    const ActiveTab = 1;
+    return (
+        <div className="tab">
+            <div className="tab__header">
+                {
+                    tabData.map((tab) => (
+                        <button key={tab.id} className={ActiveTab === tab.id ? "active" : ""}>
+                            <span>{tab.title}</span>
+                            <span className="tab-indicator"></span>
+                        </button>
+                    ))
+                }
+            </div>
+            <div className="tab__content">
+               {
+                tabData[ActiveTab - 1].content
+               }
+            </div>
+        </div>
+    )
+}
+
+export default App;
